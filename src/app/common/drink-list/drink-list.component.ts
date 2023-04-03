52eb4168efe0341d3e9e66e0eebc023cae6ecfbe
cherryApp <j.cserko@gmail.com>
2023-04-03 09:38:09 +0200

    database access

--- src/app/common/drink-list/drink-list.component.ts
+++ src/app/common/drink-list/drink-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, inject } from '@angular/core';
+import { Consume } from 'src/app/model/consume';
 import { ConsumeService } from 'src/app/service/consume.service';
 
 @Component({
@@ -10,7 +11,7 @@ export class DrinkListComponent {
 
   consumeService = inject(ConsumeService);
 
-  consumeList$ = this.consumeService.list$;
+  consumeList$ = this.consumeService.getAll();
 
   getWeekDayName(timeStamp: number): string {
     const dayNames = ['Vasárnap', 'Hétfő', 'Kedd', 'Szerda', 'Csütörtök', 'Péntek', 'Szombat', 'Vasárnap'];
@@ -21,6 +22,12 @@ export class DrinkListComponent {
 
   getFormattedDate(timeStamp: number): string {
     return new Intl.DateTimeFormat('hu-HU').format(timeStamp);
+  }
+
+  onRemoveConsume(consume: Consume): void {
+    this.consumeService.delete(consume.id).subscribe(
+      () => this.consumeList$ = this.consumeService.getAll()
+    );
   }
 
 }
--- src/app/service/consume.service.ts
+++ src/app/service/consume.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Cons, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { Consume } from '../model/consume';
 
+// Consume CRUD: Create, Read, Update, Delete
 @Injectable({
   providedIn: 'root'
 })
 export class ConsumeService {
+
+  http: HttpClient = inject(HttpClient);
+
+  apiUrl: string = environment.apiUrl;
+
+  entityName: string = 'consume';
 
   list$: Consume[] = [
     {id: 1, type: 'simple', user: 1, timeStamp: new Date().getTime(), payed: false},
@@ -15,4 +25,31 @@ export class ConsumeService {
   ];
 
   constructor() { }
+
+  getAll(): Observable<Consume[]> {
+    return this.http.get<Consume[]>(`${this.apiUrl}${this.entityName}`);
+  }
+  
+  get(id: number): Observable<Consume> {
+    return this.http.get<Consume>(`${this.apiUrl}${this.entityName}/${id}`);
+  }
+  
+  create(consume: Consume): Observable<Consume> {
+    return this.http.post<Consume>(
+      `${this.apiUrl}${this.entityName}`,
+      consume
+    );
+  }
+  
+  update(consume: Consume): Observable<Consume> {
+    return this.http.patch<Consume>(
+      `${this.apiUrl}${this.entityName}/${consume.id}`,
+      consume
+    );
+  }
+
+  delete(id: number): Observable<Consume> {
+    return this.http.delete<Consume>(`${this.apiUrl}${this.entityName}/${id}`);
+  }
+
 }
